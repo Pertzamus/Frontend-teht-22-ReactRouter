d26d4673da16f6c34ad365ba192e894d385f5661
Pertzamus <korhonen.pertti@hotmail.com>
2022-03-29 18:04:37 +0300

    läksy tehty

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+function Home() {
+  return <h2>This is Home page</h2>;
+}
+
+function About() {
+  return <h2>This is About page</h2>;
+}
+
+function Contact() {
+  return <h2>This is Contact page</h2>;
+}
+
+function NotFound() {
+  return <h2>Home</h2>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to React Router</h1>
+      <BrowserRouter>
+      <Link to="/">Home</Link>{' '}
+      <Link to="/about">About</Link>{' '}
+      <Link to="/contact">Contact</Link>{' '}
+      <Routes>
+      <Route exact path="/"element={<Home />} />
+      <Route path="/about"element={<About />} />
+      <Route path="/contact"element={<Contact />} />
+      <Route path="*" element={<NotFound />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
